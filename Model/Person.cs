9dde5a8d98c0555c4cd8e699acc886de6f3d022f
Ueberte Goncalves <ueberte.miranda@gmail.com>
2019-06-15 01:50:27 -0300

    Class Person

---
+++ Model/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Model
+{
+    public class Person
+    {
+        public long id { get; set; }
+        public string Fistname { get; set; }
+        public string Lastname { get; set; }
+        public string Adress { get; set; }
+
+        public string Gender { get; set; }
+
+
+    }
+}

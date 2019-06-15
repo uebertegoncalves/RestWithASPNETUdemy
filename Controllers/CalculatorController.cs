decd75bf7b0ab199d4cf7d97d9c70c8e33fc5ce9
Ueberte Goncalves <ueberte.miranda@gmail.com>
2019-06-15 01:29:17 -0300

    Calculator Sum

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+
+        // GET api/values/5/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+
+            }
+            return BadRequest("Invalid Input!");
+        }
+
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(number, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+
+            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+    }
+}
